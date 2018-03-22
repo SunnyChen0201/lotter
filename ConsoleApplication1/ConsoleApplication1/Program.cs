9d7c8b5d19b5c5edd2b1ae03cb0742386e872425
m305-32\w401 <sunny1349@gmail.com>
2018-03-22 18:06:48 +0800

    大樂透

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,10 +10,36 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            //產生亂數物件
             Random rand = new Random();
-            Console.WriteLine(rand.Next(50, 100));
 
-            Console.ReadLine();
+            //紀錄次數的變數
+            int n = 0;
+
+            
+
+            //產生六個亂數
+            while (n<=10)
+            {
+                int r1 = rand.Next(1, 43);
+                int r2 = rand.Next(1, 43);
+                int r3 = rand.Next(1, 43);
+                int r4 = rand.Next(1, 43);
+                int r5 = rand.Next(1, 43);
+                int r6 = rand.Next(1, 43);
+
+                //印出
+                Console.WriteLine("{0:00} {1:00} {2:00} {3:00} {4:00} {5:00} ", r1, r2, r3, r4, r5, r6);
+
+                //次數+1
+                n = n + 1;
+
+
+                //暫停螢幕
+
+                Console.ReadLine();
+            }
         }
     }
 }
